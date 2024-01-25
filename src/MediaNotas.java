fc67065478bb8d6207ed273173b86c0c0785b73f
Lucas Acosta <lucas.heck@live.com>
2024-01-25 12:19:59 -0300

    inital commit

---
+++ src/MediaNotas.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MediaNotas {
+    private static final int NUM_NOTAS = 4;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double soma = 0;
+
+        for (int i = 0; i < NUM_NOTAS; i++)
+        {
+            System.out.print("Digite a nota número " + (i + 1) + ": ");
+            double nota = scanner.nextDouble();
+            soma += nota;
+        }
+        System.out.println();
+        System.out.println("A média final foi: " + (soma / NUM_NOTAS));
+        System.out.println();
+
+        scanner.close();
+    }
+}
